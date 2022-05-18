aad78a589f6b5943ba57ec7b55d71a290086c946
GTapok <17tapo4ek17@gmail.com>
2022-05-18 23:24:34 +0300

    Loona

--- bot.py
+++ bot.py
@@ -78,10 +78,10 @@ async def video(message: types.Message):
 @dp.message_handler(commands=["profile"])
 async def profile(message: types.Message):
     if message.from_user.id == 1527663110 or message.from_user.id == 862085756:
-        re = users.cur.execute(f"SELECT * FROM users").fetchall()
-        kol = len(re)
-        users.conn.commit()
-        await message.reply(kol)
+        a = users.cur.execute(f"SELECT arts FROM users")
+        koll = len(a)
+        await message.reply(koll)
+
     else:
         for a in users.cur.execute(f"SELECT arts FROM users where id = {message.from_user.id}"):
             await message.reply(f"Content points: {a[0]}.\n"
